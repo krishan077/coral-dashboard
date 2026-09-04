d02f12f0d51bbb92a0275abd939028fba9b6a901
krishan077 <krishankant@ashmar.in>
2026-09-04 14:41:54 +0530

    api url code updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://node.monetanalytics.com/coral/coral/', 
     // apiUrl: 'http://192.168.0.122:3011/coral/',
-    nodeApiUrl: `https://nodeapi.monetanalytics.com/dashboard/api/`
+    nodeApiUrl: `https://nodeapi.monetanalytics.com/dashboard/dashboard/`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // apiUrl: 'http://192.168.0.122:3011/coral/',
     apiUrl: 'https://node.monetanalytics.com/coral/coral/',
-    nodeApiUrl: `https://nodeapi.monetanalytics.com/dashboard/api/`
+    nodeApiUrl: `https://nodeapi.monetanalytics.com/dashboard/dashboard/`
 };
